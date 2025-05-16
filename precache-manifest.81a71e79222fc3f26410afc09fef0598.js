2b9bd2a00edd2479c20c66e88a2c229d88a97cb5
Galdino <bruno2galdino@gmail.com>
2025-05-16 13:36:21 -0300

    Updates

---
+++ precache-manifest.81a71e79222fc3f26410afc09fef0598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d4a0c876eddc7068956f8828be6fc9",
+    "url": "/tenis_brasil/index.html"
+  },
+  {
+    "revision": "b3302143397232e0b24e",
+    "url": "/tenis_brasil/static/css/main.f9546b39.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/tenis_brasil/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/tenis_brasil/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "b3302143397232e0b24e",
+    "url": "/tenis_brasil/static/js/main.0b46f903.chunk.js"
+  },
+  {
+    "revision": "21efab89812fe2a74f32",
+    "url": "/tenis_brasil/static/js/runtime~main.fcc21742.js"
+  },
+  {
+    "revision": "633f6c349fb01581509bb3588f973596",
+    "url": "/tenis_brasil/static/media/logo.633f6c34.png"
+  }
+]);
